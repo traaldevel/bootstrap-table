1c05a3a1d634f3ae59e7ea7197dd8ae6e69c8368
Traal Devel <traaldevel@gmail.com>
2017-05-10 10:37:22 +0200

    :HACK: Workaround so that filterData value is used for the column-search
    Added code, so the filterDate of a specific column is used to determine
    the real value for the column-search. So far no side effects detected
    (see copyValues, onSearch, column-search). 
    fyi: Could not find a better way to access filterData for the current
    column.

--- src/extensions/filter-control/bootstrap-table-filter-control.js
+++ src/extensions/filter-control/bootstrap-table-filter-control.js
@@ -618,7 +618,22 @@
         }
 
         copyValues(this);
-        var text = $.trim($(event.currentTarget).val());
+        
+        // :HACK: [START] Workaround so that filterData value is used for the column-search.
+        // So far no side effects detected (see copyValues, onSearch, column-search).
+        // Could not find a better way to access filterData for the current column.
+        var colidx = $(event.currentTarget).parents("tr").children().index($(event.currentTarget).parents("th"));
+        var filterData = this.columns[colidx].filterData;
+        
+        var text = null;
+        if (filterData && filterData.indexOf("var:") == 0) {
+          var tmpSectionOptions = eval(filterData.replace("var:", "")); 
+          text = tmpSectionOptions[$.trim($(event.currentTarget).val())];
+        } else {
+          text = $.trim($(event.currentTarget).val());
+        }
+        // :HACK: [END]
+        
         var $field = $(event.currentTarget).closest('[data-field]').data('field');
 
         if ($.isEmptyObject(this.filterColumnsPartial)) {
